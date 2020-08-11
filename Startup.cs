dba642913329184e677b1b77d0b99c07266ca2be
Moksiy <maksaligin@gmail.com>
2020-08-11 21:33:14 +0300

    Добавьте файлы проекта.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommentsTest.Startup))]
+namespace CommentsTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
